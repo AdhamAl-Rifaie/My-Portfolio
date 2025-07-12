b402eb502468bca28f84b4244e4147522f468e9a
Adham <adham.elrefa3y@gmail.com>
2025-07-13 01:21:07 +0300

    Second Commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/', // Replace with your actual repo name
+  base: '/My-Portfolio', // Replace with your actual repo name
   server: {
     port: 5173,
     host: true,
